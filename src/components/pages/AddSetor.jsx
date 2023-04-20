27faaf9a10d765f3d7794b8dc3a03167d605fc7e
IsmaelMartins934 <ismaelmartins934@gmail.com>
2023-04-20 15:13:37 -0300

    paginas para adicionar setores

---
+++ src/components/pages/AddSetor.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+import Style from './AddSetor.module.css'
+
+import Inputs from "../form/Inputs"
+
+
+function AddSetor() {
+
+  let [setor, setSetor] = useState('')
+
+  return (
+    <Inputs
+      type='text'
+      place='o nome do setor'
+      ValueLabel='Digite aqui o nome do setor: '
+      change={e=>setSetor(e.target.value)}
+    />
+  )
+}
+
+export default AddSetor
